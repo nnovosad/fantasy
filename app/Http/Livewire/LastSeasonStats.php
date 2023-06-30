5e69d441f32471149fa03d4b9203c01d6907a226
Nikolay Novosad <nikolay.novosad@wizart.ai>
2023-06-30 21:53:50 +0300

    Add tailwind CSS and refactor views templates

--- app/Http/Livewire/LastSeasonStats.php
+++ app/Http/Livewire/LastSeasonStats.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Livewire;
 
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Foundation\Application;
 use Livewire\Component;
 
 class LastSeasonStats extends Component
 {
-    public function render()
+    public function render(): View|Application|Factory|\Illuminate\Contracts\Foundation\Application
     {
         return view('livewire.last-season-stats');
     }
