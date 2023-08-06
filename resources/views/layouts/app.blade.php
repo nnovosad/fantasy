8213ec7486621bd2d8926091544039ad7703cb21
Nikolay Novosad <nikolay.novosad@wizart.ai>
2023-08-06 13:33:36 +0300

    Add GA

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,15 @@
 <html>
 <head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-FGPR0QF50T"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-FGPR0QF50T');
+    </script>
+
     <title>@yield('title')</title>
     @livewireStyles
 
